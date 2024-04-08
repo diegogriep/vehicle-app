e0cc7729a4cf9341c9d43fea20f0d0beefedeb07
Diego Griep <fale@diegogriep.com.br>
2024-04-08 17:17:12 +0100

    chore: Removing comments

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,3 @@
-// import vehicles from './vehicles_dataset.json'
-
-// export type VehiclesProps = typeof vehicles
-// export type VehiclesItemProps = typeof vehicles[number]
-
-// export async function GET() {
-//   return Response.json(vehicles);
-// }
 import path from 'path'
 import fsPromises from 'fs/promises'
 
